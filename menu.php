70a7e35cc8dda0ff3b28a548fa8c25de8e5203f7
rodrego <rodrego@gmail.com>
2025-10-17 15:20:16 -0300

    Site - second batch

---
+++ menu.php
@@ -0,0 +1,71 @@
+<section class="col-md-3">
+    <header class="cabecalho">
+        <h1>
+            <a href="index.html"><img src="img/rodrigo-rego.png" alt="Rodrigo Rego - board game designer"></a>
+        </h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="http://www.rodrigorego.com.br/index.php">Home</a></li>
+            <li>Published Games
+                <ul>
+                    <li><a href="http://www.rodrigorego.com.br/camisa-12.php">Camisa 12</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/dead-breakfast.php">Dead &amp; Breakfast</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/etop.php">É Top!</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/emboscados.php">Emboscados</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/herdeiros-do-khan.php">Herdeiros do Khan</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/papertown.php">Papertown</a></li>
+                </ul>
+            </li>
+            <!--<li>To be released
+                <ul>
+                </ul>
+            </li>-->
+            <li>Looking for publishers
+                <ul>
+                    <!-- <li><a href="http://www.rodrigorego.com.br/dubai.php">Dubai</a></li> -->
+                    <li><a href="http://www.rodrigorego.com.br/copacabana.php">Copacabana</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/fall-of-the-ancients.php">Fall of the Ancients</a></li>
+                    <!-- <li><a href="out-of-order.php">Out of Order</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/range.php">Range</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/stranger-in-the-nest.php">Stranger in the Nest</a></li>
+                    <li><a href="http://www.rodrigorego.com.br/winter-is-coming.php">Winter is Coming</a></li> -->
+                </ul>
+            </li>
+            <li><a href="/blog">Blog (in portuguese)</a></li>
+        </ul>
+    </nav>
+    <aside>
+        <ul>
+            <li><a href="https://www.facebook.com/rodrigo.rego.14?ref=bookmarks" target="_blank">Facebook</a></li>
+            <li><a href="https://www.instagram.com/regorodrigo_/" target="_blank">Instagram</a></li>
+            <li><a href="https://www.ludopedia.com.br/designer/3666/rodrigo-rego" target="_blank">Ludopedia</a></li>
+            <li><a href="https://boardgamegeek.com/boardgamedesigner/91360/rodrigo-rego" target="_blank">BGG</a></li>
+            <li><a href="mailto:rodrego@gmail.com">rodrego@gmail.com</a></li>
+        </ul>
+    </aside>
+    <select ONCHANGE="location = this.options[this.selectedIndex].value;"> 
+        <option value="http://www.rodrigorego.com.br/menu.php">Menu</option>
+        <option value="http://www.rodrigorego.com.br/index.php">Home</option>
+        <option value="/blog">Blog (in portuguese)</option>
+        <optgroup label="Published Games">
+            <option value="http://www.rodrigorego.com.br/camisa-12.php">Camisa 12</option>
+            <option value="http://www.rodrigorego.com.br/dead-breakfast.php">Dead &amp; Breakfast</option>
+            <option value="http://www.rodrigorego.com.br/e-top.php">É Top</option>
+            <option value="http://www.rodrigorego.com.br/emboscados.php">Emboscados</option>
+            <option value="http://www.rodrigorego.com.br/herdeiros-do-khan.php">Herdeiros do Khan</option>
+            <option value="http://www.rodrigorego.com.br/papertown.php">Papertown</option>
+        </optgroup>
+        <!--<optgroup label="To be released">
+        </optgroup>-->
+        <optgroup label="Looking for publishers">
+            <!-- <option value="http://www.rodrigorego.com.br/dubai.php">Dubai</option> -->
+            <option value="http://www.rodrigorego.com.br/copacabana.php">Copacabana</option>
+            <option value="http://www.rodrigorego.com.br/fall-of-the-ancients.php">Fall of the Ancients</option>
+            <!-- <option value="http://www.rodrigorego.com.br/out-of-order.php">Out of Order</option>
+            <option value="http://www.rodrigorego.com.br/range.php">Range</option>
+            <option value="http://www.rodrigorego.com.br/stranger-in-the-nest.php">Stranger in the Nest</option>
+            <option value="http://www.rodrigorego.com.br/winter-is-coming.php">Winter is Coming</option> -->
+        </optgroup>
+    </select>
+</section>
